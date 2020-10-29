cea6f4f5a9941c7d1747d7f3af3a9a3ab2f21938
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-10-29 21:20:38 +0000

    Create twitch-ublock-userResourcesLocation.js

---
+++ twitch-ublock-userResourcesLocation.js
@@ -0,0 +1,10 @@
+if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
+var realFetch = window.fetch;
+window.fetch = function(input, init) {
+    if ( arguments.length >= 2 && typeof input === 'string' && input.includes('/access_token') ) {
+        var url = new URL(arguments[0]);
+        url.searchParams.set("player_type", "frontpage");
+        arguments[0] = url.href;
+    }
+    return realFetch.apply(this, arguments);
+};
