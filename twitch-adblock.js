54df501329e99d47d13ae8d5b352df81213fe656
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-10-29 21:18:55 +0000

    Update twitch-adblock.js

--- twitch-adblock.js
+++ twitch-adblock.js
@@ -6,52 +6,74 @@
 // @version     1.0
 // @author      if-otherwise
 // @run-at      document-idle
-// @description 10/28/2020, 11:24:10 PM
+// @description This is a script that replaces ads with a video of your choice. This is intended to be used while uBlock Origin is unable to stop ads - aka a bandaid fix.
 // ==/UserScript==
 
 (function() {
     console.log('############# Starting "Fuck Off Twitch Ads" #############');
-    if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
-    var realFetch = window.fetch;
-    window.fetch = function(input, init) {
-        if ( arguments.length >= 2 && typeof input === 'string' && input.includes('/access_token') ) {
-            var url = new URL(arguments[0]);
-            url.searchParams.set("player_type", "frontpage");
-            arguments[0] = url.href;
-        }
-        return realFetch.apply(this, arguments);
-    };
-    // If above fails, replace ad video with one of your own - because fuck ads. Catch up on a show or something 
     var video = document.querySelector('.video-player video');
     var playButton = document.querySelector('.player-controls__left-control-group > div:nth-child(1) > button:nth-child(1)');
+    var muteButton = document.querySelector('button[data-a-target="player-mute-unmute-button"]');
+    var adOverlay = document.querySelector('span[data-test-selector=ad-banner-default-text]');
     playButton.click();
-    playButton.click();
-    // twitch a bit fucky with how it loads elements. this is a super bandaid fix until I can do more testing
     setTimeout(() => {
-        var adOverlay = document.querySelector('span[data-test-selector=ad-banner-default-text]');
-        if (adOverlay !== null) {
+        playButton.click();
+        if (!!adOverlay) {
             console.log('############# Replacing Twitch Video #############');
             var userVideo = 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov'; // <-- REPLACE WITH WHATEVER HOSTED VID FILE
             video.src = userVideo;
             if (!!window.localStorage.getItem('adReplacementTime')) {
                 video.currentTime = parseFloat(window.localStorage.getItem('adReplacementTime'));
                 playButton.click();
-                playButton.click();
             } else {
                 playButton.click();
-                playButton.click();
+                var timePlayed = 0;
                 var isAdPlaying = setInterval(() => {
-                    if (adOverlay === null) {
+                    timePlayed++;
+                    if (parseFloat(document.querySelector('div.tw-c-background-overlay:nth-child(4) > span:nth-child(1)').innerText.split(':')[1]) === 5) {
+                      setTimeout(() => {
                         console.log('############# Ad Complete #############');
-                        window.localStorage.setItem('adReplacementTime', video.currentTime);
-                        clearInterval(isAdPlaying);
-                        window.location.reload();
+                        var savedTime = 0;
+                        if (!!window.localStorage.getItem('adReplacementTime')) {
+                          savedTime = parseFloat(window.localStorage.getItem('adReplacementTime')) + timePlayed;
+                          window.localStorage.setItem('adReplacementTime', savedTime);
+                        } else {
+                          window.localStorage.setItem('adReplacementTime', timePlayed);
+                        }
+                        window.location.reload()
+                      }, 5750)
                     }
                 }, 1000);
                 isAdPlaying;
             }
         } else {
           console.log('############# No Ad Present #############');
+          playButton.click();
+          muteButton.click();
+          console.log('############# Starting Watcher For Midrolls #############');
+          var isMidrollPlaying = setInterval(() => {
+              if (!!adOverlay) {
+                playButton.click();
+                muteButton.click();
+                var timePlayed = 0;
+                timePlayed++;
+                if (parseFloat(document.querySelector('div.tw-c-background-overlay:nth-child(4) > span:nth-child(1)').innerText.split(':')[1]) === 5) {
+                  setTimeout(() => {
+                    console.log('############# Ad Complete #############');
+                    var savedTime = 0;
+                    if (!!window.localStorage.getItem('adReplacementTime')) {
+                      savedTime = parseFloat(window.localStorage.getItem('adReplacementTime')) + timePlayed;
+                      window.localStorage.setItem('adReplacementTime', savedTime);
+                    } else {
+                      window.localStorage.setItem('adReplacementTime', timePlayed);
+                    }
+                    window.location.reload()
+                  }, 5750)
+                }
+              }
+          }, 1000);
+          isMidrollPlaying;
         }
     }, 5000);
 })()
+
