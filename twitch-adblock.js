18030a26d84d682530e846aca92b0d51b32b195a
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-10-29 00:25:17 +0000

    Update twitch-adblock.js

--- twitch-adblock.js
+++ twitch-adblock.js
@@ -24,30 +24,33 @@
     // If above fails, replace ad video with one of your own - because fuck ads. Catch up on a show or something 
     var video = document.querySelector('.video-player video');
     var playButton = document.querySelector('.player-controls__left-control-group > div:nth-child(1) > button:nth-child(1)');
-    var adOverlay = document.querySelector('div.tw-absolute.tw-c-background-overlay.tw-c-text-overlay.tw-inline-block.tw-left-0.tw-pd-1.tw-top-0');
-    if (adOverlay !== null) {
-        console.log('############# Replacing Twitch Video #############');
-        var userVideo = 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov'; // <-- REPLACE WITH WHATEVER HOSTED VID FILE
-        video.src = userVideo;
-        if (!!window.localStorage.getItem('adReplacementTime')) {
-            video.currentTime = parseFloat(window.localStorage.getItem('adReplacementTime'));
-            playButton.click();
-            playButton.click();
+    playButton.click();
+    playButton.click();
+    setTimeout(() => {
+        var adOverlay = document.querySelector('span[data-test-selector=ad-banner-default-text]');
+        if (adOverlay !== null) {
+            console.log('############# Replacing Twitch Video #############');
+            var userVideo = 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov'; // <-- REPLACE WITH WHATEVER HOSTED VID FILE
+            video.src = userVideo;
+            if (!!window.localStorage.getItem('adReplacementTime')) {
+                video.currentTime = parseFloat(window.localStorage.getItem('adReplacementTime'));
+                playButton.click();
+                playButton.click();
+            } else {
+                playButton.click();
+                playButton.click();
+                var isAdPlaying = setInterval(() => {
+                    if (adOverlay === null) {
+                        console.log('############# Ad Complete #############');
+                        window.localStorage.setItem('adReplacementTime', video.currentTime);
+                        clearInterval(isAdPlaying);
+                        window.location.reload();
+                    }
+                }, 1000);
+                isAdPlaying;
+            }
         } else {
-            playButton.click();
-            playButton.click();
-            var isAdPlaying = setInterval(() => {
-                if (adOverlay === null) {
-                    console.log('############# Ad Complete #############');
-                    window.localStorage.setItem('adReplacementTime', video.currentTime);
-                    clearInterval(isAdPlaying);
-                    window.location.reload();
-                }
-            }, 1000);
-            isAdPlaying;
+          console.log('############# No Ad Present #############');
         }
-    } else {
-      console.log('############# No Ad Present #############');
-      playButton.click();
-    }
+    }, 5000);
 })()
