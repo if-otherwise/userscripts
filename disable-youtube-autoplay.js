62b8ee342867040e76b138bf3afee18c41955272
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-10-29 22:49:41 +0000

    Update disable-youtube-autoplay.js

--- disable-youtube-autoplay.js
+++ disable-youtube-autoplay.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Fuck Off YouTube Autoplay (WIP)
 // @namespace   Violentmonkey Scripts
-// @match       *://*.youtube.com/*
+// @match       *://*.youtube.com/watch?v*
 // @grant       none
 // @version     1.0
 // @author      if-otherwise
@@ -11,33 +11,30 @@
 
 (function () {
     console.log('[Disable YouTube Autoplay]: Started at ' + new Date().toLocaleString());
-    var attempt = 0;
-    var stopAutoplay = setInterval(function () {
-        attempt++;
-        console.log('[Disable YouTube Autoplay]: Starting attempt [' + attempt + ']');
-        var autoplayButton = document.querySelector('paper-toggle-button[aria-label="Autoplay"]');
-        if (!!autoplayButton) {
-            if (autoplayButton.checked && autoplayButton.active) {
-                autoplayButton.click();
-                console.log('[Disable YouTube Autoplay]: Autoplay disabled!');
-                clearInterval(stopAutoplay);
-            } else {
-                console.log('[Disable YouTube Autoplay]: Autoplay already disabled!');
-                clearInterval(stopAutoplay);
-            }
-        }
-        else {
-            console.log('[Disable YouTube Autoplay]: Autoplay button not present.');
-            clearInterval(stopAutoplay);
-        }
-    }, 10000);
-    stopAutoplay;
     document.body.addEventListener("yt-navigate-finish", function(event) {
         if (location.href.includes("watch?v")) {
-            console.log('[Disable YouTube Autoplay]: Video page detected! Starting if autoplay button present. Current attempts: [' + attempt + ']');
+            console.log('[Disable YouTube Autoplay]: Started at ' + new Date().toLocaleString());
+            var attempt = 0;
+            var stopAutoplay = setInterval(function () {
+                attempt++;
+                console.log('[Disable YouTube Autoplay]: Starting attempt [' + attempt + ']');
+                var autoplayButton = document.querySelector('paper-toggle-button[aria-label="Autoplay"]');
+                if (!!autoplayButton) {
+                    if (autoplayButton.checked && autoplayButton.active) {
+                        autoplayButton.click();
+                        console.log('[Disable YouTube Autoplay]: Autoplay disabled!');
+                        clearInterval(stopAutoplay);
+                    } else {
+                        console.log('[Disable YouTube Autoplay]: Autoplay already disabled!');
+                        clearInterval(stopAutoplay);
+                    }
+                }
+                else {
+                    console.log('[Disable YouTube Autoplay]: Autoplay button not present.');
+                    clearInterval(stopAutoplay);
+                }
+            }, 10000);
             stopAutoplay;
-        } else {
-            clearInterval(stopAutoplay);
         }
     });
 })();
