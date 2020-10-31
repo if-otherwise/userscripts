1c1680e8cc22ccbdfeb0049835428424deda5154
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-10-31 21:34:03 +0000

    Update twitch-ublock-userResourcesLocation.js

--- twitch-ublock-userResourcesLocation.js
+++ twitch-ublock-userResourcesLocation.js
@@ -1,20 +1,19 @@
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
-    if (typeof url === "string") {
-        if (url.includes("/access_token")) {
-            const newUrl = new URL(arguments[0]);
-            newUrl.searchParams.set("player_type", "dashboard");
-            url = url.href;
-        } else if (
-            url.includes("/gql") &&
-            init &&
-            typeof init.body === "string" &&
-            init.body.includes("PlaybackAccessToken")
-        ) {
-            const newBody = JSON.parse(init.body);
-            newBody.variables.playerType = "dashboard";
-            init.body = JSON.stringify(newBody);
-        }
-    }
-    return origFetch(url, init, ...args);
+	if (typeof url === "string") {
+		if (url.includes("/access_token")) {
+			url = url.replace("player_type=site", "player_type=embed");
+		} else if (
+			url.includes("/gql") &&
+			init &&
+			typeof init.body === "string" &&
+			init.body.includes("PlaybackAccessToken")
+		) {
+			const newBody = JSON.parse(init.body);
+			newBody.variables.playerType = "embed";
+			init.body = JSON.stringify(newBody);
+		}
+	}
+	return origFetch(url, init, ...args);
 };
+
