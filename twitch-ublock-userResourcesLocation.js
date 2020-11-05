b4ba8a8e33ecdbbbf76f12abf13e67d314c5eba8
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-11-05 23:56:41 +0000

    Update twitch-ublock-userResourcesLocation.js

--- twitch-ublock-userResourcesLocation.js
+++ twitch-ublock-userResourcesLocation.js
@@ -1,8 +1,9 @@
+// twitch-ublock-userResourcesLocation.js
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
-			url = url.replace("player_type=site", "player_type=embed");
+			url = url.replace("player_type=site", "player_type=site");
 		} else if (
 			url.includes("/gql") &&
 			init &&
@@ -10,10 +11,9 @@ window.fetch = (url, init, ...args) => {
 			init.body.includes("PlaybackAccessToken")
 		) {
 			const newBody = JSON.parse(init.body);
-			newBody.variables.playerType = "embed";
+			newBody.variables.playerType = "site";
 			init.body = JSON.stringify(newBody);
 		}
 	}
 	return origFetch(url, init, ...args);
 };
-
