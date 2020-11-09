730e095ab2e2c3ef1524e6f27982a3fa2c29ab1d
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-11-09 01:03:59 +0000

    Update thotblock.js

--- thotblock.js
+++ thotblock.js
@@ -6,16 +6,16 @@
 // @connect     gist.githubusercontent.com
 // @updateURL   https://raw.githubusercontent.com/if-otherwise/violentmonkey-scripts/main/thotblock.js
 // @downloadURL https://raw.githubusercontent.com/if-otherwise/violentmonkey-scripts/main/thotblock.js
-// @version     1.1
+// @version     1.11
 // @author      if-otherwise
 // @run-at      document-idle
-// @description Annoyed of seeing softcore porn thumbnails of girls making a living on simp money 
+// @description Annoyed of seeing softcore porn thumbnails of girls making a living on simp money
 //              when you're just looking to find an interesting stream? 
 //              Have no fear, ThotBlock is here!
 // ==/UserScript==
 
-(function() { 
-  
+(function() {
+
     console.log('Initiating ThotBlock!');
 
     let thots = [];
@@ -25,7 +25,7 @@
     let thotListJson = 'https://gist.githubusercontent.com/if-otherwise/41ba8833138245cab13e2bc19f833b0d/raw/blocklist.json?cacheprevent='+(new Date).getTime();
 
     // This is specific to violentmonkey (https://violentmonkey.github.io). 
-    // Will also work for tampermonkey
+    // Tamper/grease have a bit different formatting and will not work.
     GM_xmlhttpRequest({
       method:         'GET',
       url:            thotListJson,
@@ -35,47 +35,38 @@
       ontimeout:      reportError,
       onabort:        reportError
     });
-  
-    // Uncomment the below function for use with greasemonkey, and comment out the above GM_xmlhttpRequest function
-    // You'll also need to change @grant to GM.xmlHttpRequest
-    /*
-    GM.xmlHttpRequest({
-      method:         'GET',
-      url:            thotListJson,
-      responseType:   'json',
-      onload:         setThotList,
-      onerror:        reportError,
-      ontimeout:      reportError,
-      onabort:        reportError
-    });
-    */
-    
+
     let thotsRetrieved = false;
     function setThotList(data) {
       try {
         thots = data.response;
         thotsRetrieved = true;
-        console.log('Successfully retrieved Thot List from Github!');        
+        console.info('Successfully retrieved Thot List from Github!');    
+        console.info(thots);
       }
       catch(e) {
         console.error('ThotBlock Error: ' + e.message)
       }
     }
 
-    function reportError(data) {
-      alert('Error retrieving Thot List! The github gist may be inaccessible or the request timed out (usually because your internet is bad).')
+    function reportError() {
+      let userResonse = confirm('Error retrieving thot list!\nGithub may be inaccessible or the request timed out.\nClick OK to refresh and try again.');
+      if (userResonse == true) {
+        window.location.reload()
+      } else {
+        console.error('Error retrieving thot list!\nGithub may be inaccessible or the request timed out.\nRefresh the page and try again.')
+      }
     }
     
     function deleteThots () {
       // changing to a location.href if statement to account for the way the site loads
       // this is instead of the UserScript @match for /directory/game/
-      if (!!location.href.includes("/directory/game/")) {
+      if (!!location.href.includes('/directory/game/')) {
         let allNodes = document.querySelectorAll('a[data-a-target="preview-card-channel-link"]');
         if (allNodes.length && thots.length) {
           allNodes.forEach((node) => {
             if (thots.includes(node.innerText) || thots.includes(node.innerText.toLowerCase())) {
-              thots = thots.filter(e => e !== node.innerText);
-              console.log('Removing Thot: ' + node.innerText);
+              console.info('Removing Thot: ' + node.innerText);
               let count = 0;
               let els = [];
               while (count < 12) {
@@ -87,9 +78,11 @@
             }
           });
         } else if ((thotsRetrieved && !thots) || (thotsRetrieved && !thots.length)) {
-          let userResonse = confirm("Thot List is empty!\nThere may have been an error, or the remote list is inaccessible.\nSelect OK to refresh and try again.");
+          let userResonse = confirm('Thot List is empty!\nThere may have been an error, or the remote list is inaccessible.\nSelect OK to refresh and try again.');
           if (userResonse == true) {
             window.location.reload()
+          } else {
+            console.error('Thot List is empty!\nThere may have been an error, or the remote list is inaccessible.\nRefresh the page and try again.')
           }
         }
       }
@@ -99,5 +92,6 @@
 
     setInterval(() => {
       deleteThots();
-    }, 2500)
+    }, 2500);
+  
 })();
