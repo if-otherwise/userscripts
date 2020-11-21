bde4c12a144e23c42be517cf9469fe04968dc5d5
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-11-21 20:26:07 +0000

    Update disable-youtube-autoplay.js

--- disable-youtube-autoplay.js
+++ disable-youtube-autoplay.js
@@ -1,35 +1,33 @@
 // ==UserScript==
-// @name        Fuck Off YouTube Autoplay (WIP)
+// @name        Fuck Off YouTube Autoplay
 // @namespace   Violentmonkey Scripts
-// @match       *://*.youtube.com/watch?v*
+// @match       *://*.youtube.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      if-otherwise
 // @run-at      document-idle
-// @description 10/28/2020, 11:46:23 PM
+// @description stops yt autoplay
 // ==/UserScript==
 
 (function () {
     console.log('[Disable YouTube Autoplay]: Started at ' + new Date().toLocaleString());
     var attempt = 0;
     var stopAutoplay = setInterval(function () {
+      if (location.href.includes("watch?v")) {
         attempt++;
         console.log('[Disable YouTube Autoplay]: Starting attempt [' + attempt + ']');
         var autoplayButton = document.querySelector('paper-toggle-button[aria-label="Autoplay"]');
         if (!!autoplayButton) {
-            if (autoplayButton.checked && autoplayButton.active) {
-                autoplayButton.click();
-                console.log('[Disable YouTube Autoplay]: Autoplay disabled!');
-                clearInterval(stopAutoplay);
-            } else {
-                console.log('[Disable YouTube Autoplay]: Autoplay already disabled!');
-                clearInterval(stopAutoplay);
-            }
-        }
-        else {
-            console.log('[Disable YouTube Autoplay]: Autoplay button not present.');
+          if (autoplayButton.checked && autoplayButton.active) {
+            autoplayButton.click();
+            console.log('[Disable YouTube Autoplay]: Autoplay disabled!');
             clearInterval(stopAutoplay);
+          } else {
+            console.log('[Disable YouTube Autoplay]: Autoplay already disabled!');
+            clearInterval(stopAutoplay);
+          }
         }
+      }
     }, 1000);
     stopAutoplay;
 })();
