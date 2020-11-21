2111a64c658f1dea2613356450a9c8830e44f3f1
if-otherwise <7328520+if-otherwise@users.noreply.github.com>
2020-11-21 20:22:59 +0000

    Update disable-yt-recommendations.js

--- disable-yt-recommendations.js
+++ disable-yt-recommendations.js
@@ -1,21 +1,42 @@
 // ==UserScript==
-// @name        Remove YouTube Recommendations
+// @name        Disable Recommendations
 // @namespace   Violentmonkey Scripts
-// @match       *://*.youtube.com/watch?v*
+// @match       *://*.youtube.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @run-at      document-idle
 // @author      if-otherwise
 // @description bye bye rabbit hole
 // ==/UserScript==
 
 (function(){
-  setTimeout(() => {
-    if (!document.getElementById('secondary')) {
-      document.getElementById('related').remove()
-    } else {
-      document.getElementById('secondary').remove();
-      document.querySelector('button[aria-label="Theater mode (t)"]').click()
-    }
-  }, 1000)
+    console.log('[Disable YouTube Recommendations]: Started at ' + new Date().toLocaleString());
+    let attempt = 0;
+    let stopRecommendations = setInterval(function () {
+      if (location.href.includes("watch?v")) {
+        setTimeout(() => {
+          attempt++;
+          console.info('[Disable YouTube Recommendations]: Starting attempt [' + attempt + ']');
+          let secondaryDiv = document.getElementById('secondary');
+          let relatedDiv = document.getElementById('related');
+          let theaterBtn = document.querySelector('button[aria-label="Theater mode (t)"]');
+          if (!!secondaryDiv) {
+            secondaryDiv.remove();
+            if (!!theaterBtn) {
+              theaterBtn.click();
+            }
+            console.info('[Disable YouTube Recommendations]: Recommendations disabled!');
+            clearInterval(stopRecommendations);
+          } else if (!!relatedDiv) {
+            relatedDiv.remove();
+            if (!!theaterBtn) {
+              theaterBtn.click();
+            }
+            console.info('[Disable YouTube Recommendations]: Recommendations disabled!');
+            clearInterval(stopRecommendations);
+          }
+        }, 1500)
+      }
+    }, 1000);
+    stopRecommendations;
 })()
